7897ac383c150c02ac4fa643a8429e26eda361bb
Txori <81823926+txoriurdina@users.noreply.github.com>
2023-04-04 09:48:13 -0400

    Update quickedit.py

--- quickedit.py
+++ quickedit.py
@@ -1,10 +1,11 @@
 import os
 from tkinter import *
 from tkinter import filedialog
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageFilter, ImageTk
 from rembg import remove
 
-# Define color schemes
+#---------------------------------------COLORS AND THEME---------------------------------------
+
 light_bg_color = "white"
 dark_bg_color = "#1E1E1E"
 light_text_color = "black"
@@ -16,7 +17,6 @@ dark_active_color_fg = "white"
 light_theme = "Light theme"
 dark_theme = "Dark theme"
 
-# Define toggle function to switch between light and dark modes
 def toggle_theme():
     global is_dark_mode
     is_dark_mode = not is_dark_mode
@@ -27,65 +27,187 @@ def toggle_theme():
     theme = light_theme if is_dark_mode else dark_theme
     root.config(bg=bg_color)
     frame.config(bg=bg_color)
+    left_side.config(bg=bg_color)
+    right_side.config(bg=bg_color)
+    bottom_left_side.config(bg=bg_color)
+    bottom_right_side.config(bg=bg_color)
+    save_button.config(bg=bg_color, fg=text_color, activebackground=active_color, activeforeground=active_color_fg)
+    browsing_box.config(bg=bg_color)
     title.config(bg=bg_color, fg=text_color)
+    image.config(bg=bg_color)
     input_path_entry.config(bg=bg_color, fg=text_color)
     browse_button.config(bg=bg_color, fg=text_color, activebackground=active_color, activeforeground=active_color_fg)
+    reset_button.config(bg=bg_color, fg=text_color, activebackground=active_color, activeforeground=active_color_fg)
     remove_bg_button.config(bg=bg_color, fg=text_color, activebackground=active_color, activeforeground=active_color_fg)
     invert_colors_button.config(bg=bg_color, fg=text_color, activebackground=active_color, activeforeground=active_color_fg)
     desaturate_button.config(bg=bg_color, fg=text_color, activebackground=active_color, activeforeground=active_color_fg)
+    flip_button.config(bg=bg_color, fg=text_color, activebackground=active_color, activeforeground=active_color_fg)
+    rotate_button.config(bg=bg_color, fg=text_color, activebackground=active_color, activeforeground=active_color_fg)
+    rotate_entry.config(bg=bg_color, fg=text_color)
+    filter_button.config(bg=bg_color, fg=text_color, activebackground=active_color, activeforeground=active_color_fg)
+    filter_options.config(bg=bg_color, fg=text_color)
     status_label.config(bg=bg_color, fg=text_color)
-    toggle_button.config(text=theme, bg=bg_color, fg=text_color, activebackground=active_color, activeforeground=active_color_fg)
+
+#---------------------------------------IMAGE EDIT FUNCTIONS AND BROWSE FILES---------------------------------------
 
 def browse_file():
-    # Open a file dialog
-    default_dir = os.path.expanduser("~/Pictures") # path to default images directory
+    global img, img_width
+    default_dir = os.path.expanduser("~/Pictures")
     file_path = filedialog.askopenfilename(
         initialdir=default_dir,
         title="Select a file",
         filetypes=[("Image Files", "*.png *.jpg *.jpeg")]
     )
-    # Insert selected file path in the entry field
     input_path_entry.delete(0, 'end')
     input_path_entry.insert(0, file_path)
 
-def remove_bg():
-    # Get input and output paths from the entry fields
-    input_path = input_path_entry.get()
-    name = input_path.split(".")
-    filename = name[0]
-    output_path = f'{filename} - removed_bg.png'
+    output_path = r"{}".format(file_path.replace("/", "\\"))
+    img = Image.open(output_path)
+    aspect_ratio = img.size[1] / img.size[0]  # calculate aspect ratio
+    new_width = img_width
+    new_height = int(new_width * aspect_ratio)
+    resized_img = img.resize((new_width, new_height), Image.ANTIALIAS) 
+    img_tk = ImageTk.PhotoImage(resized_img) 
+    root.img_tk = img_tk
+    image.config(image=img_tk)
 
-    input = Image.open(input_path)
-
-    output = remove(input)
+def save():
+    global output_path, output
     output.save(output_path)
     status_label.configure(text="File saved as " + output_path)
+    file_path = status_label.cget("text")[14:]
+    os.startfile(file_path)
 
+def reset():
+    global output, output_path
+    output, output_path = None, None
+    input_path = input_path_entry.get()
+    img = Image.open(input_path)
+    aspect_ratio = img.size[1] / img.size[0]  # calculate aspect ratio
+    new_width = img_width
+    new_height = int(new_width * aspect_ratio)
+    resized_img = img.resize((new_width, new_height), Image.ANTIALIAS) 
+    img_tk = ImageTk.PhotoImage(resized_img) 
+    root.img_tk = img_tk
+    image.config(image=img_tk)
 
-def desaturate():
+def remove_bg():
+    global img, img_width, output, output_path
     input_path = input_path_entry.get()
     name = input_path.split(".")
     filename = name[0]
-    output_path = f'{filename} - desaturated.png'
-    print(input_path)
+    output_path = rf'{filename} - removed_bg.png'
     input = Image.open(input_path)
-    print(input)
+    output = remove(input)
+
+    img = output
+    aspect_ratio = img.size[1] / img.size[0]  # calculate aspect ratio
+    new_width = img_width
+    new_height = int(new_width * aspect_ratio)
+    resized_img = img.resize((new_width, new_height), Image.ANTIALIAS) 
+    img_tk = ImageTk.PhotoImage(resized_img) 
+    root.img_tk = img_tk
+    image.config(image=img_tk)
+
+def grayscale():
+    global img, img_width, output, output_path
+    input_path = input_path_entry.get()
+    name = input_path.split(".")
+    filename = name[0]
+    output_path = rf'{filename} - grayscale.png'
+    input = Image.open(input_path)
     output = ImageOps.grayscale(input)
-    output.save(output_path)
-    status_label.config(text="File saved as " + output_path)
+
+    img = output
+    aspect_ratio = img.size[1] / img.size[0]  # calculate aspect ratio
+    new_width = img_width
+    new_height = int(new_width * aspect_ratio)
+    resized_img = img.resize((new_width, new_height), Image.ANTIALIAS) 
+    img_tk = ImageTk.PhotoImage(resized_img) 
+    root.img_tk = img_tk
+    image.config(image=img_tk)
 
 def invert_colors():
+    global img, img_width, output, output_path
     input_path = input_path_entry.get()
     name = input_path.split(".")
     filename = name[0]
-    output_path = f'{filename} - inverted_colors.png'
+    output_path = rf'{filename} - inverted_colors.png'
 
     input = Image.open(input_path)
     output = ImageOps.invert(input)
-    output.save(output_path)
-    status_label.config(text="File saved as " + output_path)
+
+    img = output
+    aspect_ratio = img.size[1] / img.size[0]  # calculate aspect ratio
+    new_width = img_width
+    new_height = int(new_width * aspect_ratio)
+    resized_img = img.resize((new_width, new_height), Image.ANTIALIAS) 
+    img_tk = ImageTk.PhotoImage(resized_img) 
+    root.img_tk = img_tk
+    image.config(image=img_tk)
+
+def flip():
+    global img, img_width, output, output_path
+    input_path = input_path_entry.get()
+    name = input_path.split(".")
+    filename = name[0]
+    output_path = rf'{filename} - flip.png'
+
+    im = Image.open(input_path)
+    output = im.transpose(Image.FLIP_LEFT_RIGHT)
+
+    img = output
+    aspect_ratio = img.size[1] / img.size[0]  # calculate aspect ratio
+    new_width = img_width
+    new_height = int(new_width * aspect_ratio)
+    resized_img = img.resize((new_width, new_height), Image.ANTIALIAS) 
+    img_tk = ImageTk.PhotoImage(resized_img) 
+    root.img_tk = img_tk
+    image.config(image=img_tk)
+
+def rotate():
+    global img, img_width, output, output_path
+    input_path = input_path_entry.get()
+    degree = float(rotate_entry.get())
+    name = input_path.split(".")
+    filename = name[0]
+    output_path = rf'{filename} - rotate.png'
+
+    input = Image.open(input_path)
+    output = input.rotate(degree)
+
+    img = output
+    aspect_ratio = img.size[1] / img.size[0]  # calculate aspect ratio
+    new_width = img_width
+    new_height = int(new_width * aspect_ratio)
+    resized_img = img.resize((new_width, new_height), Image.ANTIALIAS) 
+    img_tk = ImageTk.PhotoImage(resized_img) 
+    root.img_tk = img_tk
+    image.config(image=img_tk)
+
+def apply_filter():
+    global img, img_width, output, output_path
+    input_path = input_path_entry.get()
+    filter_option = variable.get()
+    name = input_path.split(".")
+    filename = name[0]
+    output_path = rf'{filename} - filter {filter_option}.png'
+    apply = getattr(ImageFilter, filter_option)
+
+    input = Image.open(input_path)
+    output = input.filter(apply)
+
+    img = output
+    aspect_ratio = img.size[1] / img.size[0]  # calculate aspect ratio
+    new_width = img_width
+    new_height = int(new_width * aspect_ratio)
+    resized_img = img.resize((new_width, new_height), Image.ANTIALIAS) 
+    img_tk = ImageTk.PhotoImage(resized_img) 
+    root.img_tk = img_tk
+    image.config(image=img_tk)
     
-# Set dark mode as default
+#---------------------------------------SETUP---------------------------------------
+
 is_dark_mode = True
 bg_color = dark_bg_color
 text_color = dark_text_color
@@ -93,58 +215,130 @@ active_color = dark_active_color
 active_color_fg = dark_active_color_fg
 theme = light_theme
 
+
+global img, img_width, output, output_path
+img = None
+img_width = 400
+output = None
+output_path = None
+
 root = Tk()
 root.title("Quick image edit")
 root.config(bg=dark_bg_color)
-root.minsize(500, 400)
-# Get the absolute path to the .ico file
+root.minsize(590, 650)
 script_dir = os.path.dirname(os.path.abspath(__file__))
 icon_path = os.path.join(script_dir, "quickedit.ico")
-
-# Use the absolute path to set the icon
 root.iconbitmap(icon_path)
 
+#---------------------------------------LAYOUT OF THE APP---------------------------------------
+
 frame = Frame(root, bg=bg_color)
-frame.grid(row=0, column=0, padx=5, pady=5)
+frame.grid(row=0, column=0)
 
 title = Label(frame, text="Quick image edit", font=("Arial", 24), bg=bg_color, fg=text_color)
-title.grid(row=0, column=1, padx=5, pady=20, sticky="ew")
+title.grid(row=0, column=0, pady=20, sticky="ew")
 
-# Create a label and entry field for input file path
-browse_button = Button(frame, text="Choose file", relief="raised", activebackground=active_color, activeforeground=active_color_fg, command=browse_file, bg=bg_color, fg=text_color)
-browse_button.grid(row=1, column=1, padx=5, pady=(5,20))
-input_path_entry = Entry(frame, width=25, bg=bg_color, fg=text_color)
-input_path_entry.grid(row=1, column=1, padx=5, pady=(60,20))
+left_side = Frame(frame, bg=bg_color)
+left_side.grid(row=1, column=0, padx=5)
 
-# Create a button to remove the background
-remove_bg_button = Button(frame, text="Remove Background", width=20, relief="raised", activebackground=active_color, activeforeground=active_color_fg, command=remove_bg, bg=bg_color, fg=text_color)
-remove_bg_button.grid(row=2, column=1, padx=5, pady=5)
-# Create a button to invert colors
-invert_colors_button = Button(frame, text="Invert Colors", width=20, relief="raised", activebackground=active_color, activeforeground=active_color_fg, command=invert_colors, bg=bg_color, fg=text_color)
-invert_colors_button.grid(row=3, column=1, padx=5, pady=5)
-# Create a button to desaturate
-desaturate_button = Button(frame, text="Desaturate", width=20, relief="raised", activebackground=active_color, activeforeground=active_color_fg, command=desaturate, bg=bg_color, fg=text_color)
-desaturate_button.grid(row=4, column=1, padx=5, pady=5)
+right_side = Frame(frame, bg=bg_color, width=100)
+right_side.grid(row=1, column=1, sticky="en")
+
+bottom_left_side = Frame(frame, bg=bg_color)
+bottom_left_side.grid(row=2, column=0, sticky="w")
+
+bottom_right_side = Frame(frame, bg=bg_color, width=100)
+bottom_right_side.grid(row=2, column=1)
 
 # Create a label to display the status message
-status_label = Label(frame, text="", bg=bg_color, fg=text_color)
-status_label.grid(row=5, column=1, padx=5, pady=5)
+status_label = Label(bottom_left_side, text="", bg=bg_color, fg=text_color, cursor="hand2")
+status_label.grid(row=0, column=0, padx=5, pady=5)
+def open(event):
+    file_path = status_label.cget("text")[14:]
+    os.startfile(file_path)
+status_label.bind("<Button-1>", open)
 
-# Create a button to toggle between light and dark modes
-toggle_button = Button(frame, text=theme, relief="raised", activebackground=active_color, activeforeground=active_color_fg, command=toggle_theme, bg=bg_color, fg=text_color)
-toggle_button.grid(row=6, column=1, padx=5, pady=(5, 20))
+save_button = Button(bottom_right_side, text="SAVE", relief="raised", width=20, activebackground=active_color, activeforeground=active_color_fg, command=save, bg=bg_color, fg=text_color)
+save_button.grid(row=0, column=0, sticky="e")
+
+#root_width = root.winfo_width()
+#root_height = root.winfo_height()
+
+#------------------------LEFT SIDE------------------------
+
+# Create a label and entry field for input file path
+browsing_box = Frame(left_side, bg=bg_color, width=img_width)
+browsing_box.grid(row=2, column=0, sticky="w")
+browse_button_width = 15
+browse_button = Button(browsing_box, text="Choose file", relief="raised", width=browse_button_width, activebackground=active_color, activeforeground=active_color_fg, command=browse_file, bg=bg_color, fg=text_color)
+browse_button.grid(row=0, column=0, padx=(0, 5), pady=5)
+input_path_entry = Entry(browsing_box, width=46, bg=bg_color, fg=text_color)
+input_path_entry.grid(row=0, column=1, padx=0, pady=5)
+
+#preview the image
+image_path = os.path.join(script_dir, "quickedit.ico")
+img = Image.open(image_path)
+aspect_ratio = img.size[1] / img.size[0]  # calculate aspect ratio
+new_width = img_width
+new_height = int(new_width * aspect_ratio)
+resized_img = img.resize((new_width, new_height), Image.ANTIALIAS) 
+img_tk = ImageTk.PhotoImage(resized_img) 
+image = Label(left_side, image=img_tk, borderwidth=0, highlightthickness=0, bg=bg_color)
+image.grid(row=3, column=0)
 
 
-# center the frame horizontally and vertically within the root window
+#------------------------RIGHT SIDE------------------------
+
+# Create a button to remove the background
+reset_button = Button(right_side, text="Reset", width=20, relief="raised", activebackground=active_color, activeforeground=active_color_fg, command=reset, bg=bg_color, fg=text_color)
+reset_button.grid(row=1, column=1, padx=5, pady=5)
+
+# Create a button to remove the background
+remove_bg_button = Button(right_side, text="Remove Background", width=20, relief="raised", activebackground=active_color, activeforeground=active_color_fg, command=remove_bg, bg=bg_color, fg=text_color)
+remove_bg_button.grid(row=2, column=1, padx=5, pady=5)
+
+# Create a button to invert colors
+invert_colors_button = Button(right_side, text="Invert Colors", width=20, relief="raised", activebackground=active_color, activeforeground=active_color_fg, command=invert_colors, bg=bg_color, fg=text_color)
+invert_colors_button.grid(row=3, column=1, padx=5, pady=5)
+
+# Create a button to desaturate
+desaturate_button = Button(right_side, text="Desaturate", width=20, relief="raised", activebackground=active_color, activeforeground=active_color_fg, command=grayscale, bg=bg_color, fg=text_color)
+desaturate_button.grid(row=4, column=1, padx=5, pady=5)
+
+# Create a button to flip
+flip_button = Button(right_side, text="Flip", width=20, relief="raised", activebackground=active_color, activeforeground=active_color_fg, command=flip, bg=bg_color, fg=text_color)
+flip_button.grid(row=5, column=1, padx=5, pady=5)
+
+# Create a button to rotate
+def clear_placeholder(event):
+    rotate_entry.delete(0, END)
+rotate_button = Button(right_side, text="Rotate", width=20, relief="raised", activebackground=active_color, activeforeground=active_color_fg, command=rotate, bg=bg_color, fg=text_color)
+rotate_button.grid(row=6, column=1, padx=5, pady=5)
+rotate_entry = Entry(right_side, width=24, bg=bg_color, fg=text_color)
+rotate_entry.grid(row=7, column=1, padx=0, pady=(5, 10))
+rotate_entry.bind("<FocusIn>", clear_placeholder)
+rotate_entry.insert(0, "Enter angle to rotate...")
+
+# Create a button to filter
+filter_button = Button(right_side, text="Filter", width=20, relief="raised", activebackground=active_color, activeforeground=active_color_fg, command=apply_filter, bg=bg_color, fg=text_color)
+filter_button.grid(row=8, column=1, padx=5, pady=5)
+choices = ["BLUR", "CONTOUR", "DETAIL", "EDGE_ENHANCE", "EDGE_ENHANCE_MORE", "EMBOSS", "FIND_EDGES", "SHARPEN", "SMOOTH", "SMOOTH_MORE"]
+variable = StringVar(right_side)
+variable.set('Choose your filter')
+filter_options = OptionMenu(right_side, variable, *choices)
+filter_options.config(width=18, relief="raised", borderwidth=2, highlightthickness=0, activebackground=active_color, activeforeground=active_color_fg, bg=bg_color, fg=text_color)
+filter_options.grid(row=9, column=1, padx=5, pady=(5, 10))
+
+
+# center the frame horizontally and vertically
 root.grid_rowconfigure(0, weight=1)
 root.grid_columnconfigure(0, weight=1)
 
-# this will center the frame both horizontally and vertically within the root window
+# center the frame both horizontally and vertically
 frame.grid_rowconfigure(0, weight=1)
 frame.grid_columnconfigure(0, weight=1)
 
-def do_nothing():
-  pass
+#---------------------------------------MENU BAR---------------------------------------
 
 def quit():
   root.destroy()
@@ -163,6 +357,11 @@ file_menu.add_command(label="Open", command=browse_file)
 file_menu.add_separator()
 file_menu.add_command(label="Exit", command=quit)
 menu_bar.add_cascade(label="File", menu=file_menu)
+
+# create a view menu
+view_menu = Menu(menu_bar, tearoff=0)
+view_menu.add_command(label="Theme", command=toggle_theme)
+menu_bar.add_cascade(label="View", menu=view_menu)
 
 # create a help menu
 help_menu = Menu(menu_bar, tearoff=0)
